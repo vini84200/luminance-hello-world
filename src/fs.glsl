1ad3bf2cb550b3d248370e2ecaac62d989741e3d
vini84200 <vini84200@debian>
2020-11-17 11:35:32 -0300

    Triangle!!

---
+++ src/fs.glsl
@@ -0,0 +1,7 @@
+in vec3 v_color;
+
+out vec3 frag_color;
+
+void main() {
+    frag_color = v_color;
+}
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,51 @@ use luminance::context::GraphicsContext;
 use luminance::pipeline::PipelineState;
 use luminance_glfw::GlfwSurface;
 use luminance_windowing::{WindowDim, WindowOpt};
+use luminance_derive::{Semantics, Vertex};
+use luminance::tess::Mode;
+use luminance::shader::Program;
+use luminance::render_state::RenderState;
 use std::process::exit;
 use std::time::Instant;
+
+
+const VS_STR: &str = include_str!("vs.glsl");
+const FS_STR: &str = include_str!("fs.glsl");
+
+#[derive(Clone, Copy, Debug, Semantics)]
+pub enum Semantics {
+  #[sem(name = "position", repr = "[f32; 2]", wrapper = "VertexPosition")]
+  Position,
+  #[sem(name = "color", repr = "[u8; 3]", wrapper = "VertexRGB")]
+  Color,
+}
+
+
+#[derive(Clone, Copy, Debug, Vertex)]
+#[vertex(sem = "Semantics")]
+pub struct Vertex {
+  #[allow(dead_code)]
+  position: VertexPosition,
+
+  #[allow(dead_code)]
+  #[vertex(normalized = "true")]
+  color: VertexRGB,
+}
+
+const VERTICES: [Vertex; 3] = [
+    Vertex::new(
+        VertexPosition::new([-0.5, -0.5]),
+        VertexRGB::new([255,0,0])
+    ),
+    Vertex::new(
+        VertexPosition::new([0.5, -0.5]),
+        VertexRGB::new([0,255,0])
+    ),
+    Vertex::new(
+        VertexPosition::new([0., 0.5]),
+        VertexRGB::new([0,0,255])
+    )
+];
 
 fn main() {
     let dim = WindowDim::Windowed {
@@ -30,6 +73,17 @@ fn main_loop(mut surface: GlfwSurface) {
     let start_t = Instant::now();
     let back_buffer = surface.back_buffer().unwrap();
 
+    let triangle = surface.new_tess()
+        .set_vertices(&VERTICES[..])
+        .set_mode(Mode::Triangle)
+        .build()
+        .unwrap();
+
+    let mut program = surface.new_shader_program::<Semantics, (), ()>()
+        .from_strings(VS_STR, None, None, FS_STR)
+        .unwrap()
+        .ignore_warnings();
+        
     'app: loop {
         surface.window.glfw.poll_events();
         for (_, event) in surface.events_rx.try_iter() {
@@ -40,17 +94,23 @@ fn main_loop(mut surface: GlfwSurface) {
         }
 
         // Rendering Code
+
         let t = start_t.elapsed().as_millis() as f32 * 1e-3;
         let color = [t.cos(), t.sin(), 0.5, 1.];
 
         let render = surface.new_pipeline_gate().pipeline(
             &back_buffer,
             &PipelineState::default().set_clear_color(color),
-            |_, _| Ok(()),
+            |_, mut shd_gate| {
+                shd_gate.shade(&mut program, |_, _, mut rdr_gate| {
+                    rdr_gate.render(&RenderState::default(), |mut tess_gate| {
+                        tess_gate.render(&triangle)
+                    })
+                })
+            },
         ).assume();
 
         if render.is_ok() {
-
             surface.window.swap_buffers();
         } else {
             break 'app;
